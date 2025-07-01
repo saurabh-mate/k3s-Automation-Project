1bd9b0cad6c50aa0d9d3ec7071b851109c5ccf4b
saurabh <saurabhmate226@gmail.com>
2025-07-01 12:52:11 +0530

    Initial commit with infrastructure automation setup

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+cd terraform
+terraform init
+terraform apply -auto-approve
+
+INSTANCE_IP=$(terraform output -raw instance_public_ip)
+echo "EC2 Public IP: $INSTANCE_IP"
+
+cd ..
+
+echo " Updating Ansible inventory"
+cat > ansible/inventory.ini <<EOF
+[server]
+$INSTANCE_IP ansible_user=ubuntu ansible_ssh_private_key_file=/home/saurabh/Downloads/Pem-files/k3s.pem
+EOF
+
+echo " Waiting for SSH to become available..."
+while ! ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no -i "/home/saurabh/Downloads/Pem-files/k3s.pem" ubuntu@$INSTANCE_IP exit 2>/dev/null; do   
+  echo "SSH not ready, retrying in 5 seconds..."
+  sleep 5
+done
+echo " SSH connection established."
+
+echo "Step 2: Running Ansible playbook to install K3s"
+ansible-playbook -i ansible/inventory.ini ansible/install_k3s.yaml
+
+echo "Installing cert-manager..."
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
+
+echo "Waiting for cert-manager components to be ready..."
+kubectl rollout status deployment/cert-manager -n cert-manager --timeout=120s
+kubectl rollout status deployment/cert-manager-webhook -n cert-manager --timeout=120s
+kubectl rollout status deployment/cert-manager-cainjector -n cert-manager --timeout=120s
+
+kubectl create namespace my-app || echo "Namespace 'my-app' already exists"
+
+echo "Step 3: Deploying K8s application"
+kubectl apply -f k3s-files/letsencrypt-prod.yaml
+kubectl apply -f k3s-files/traefik-https-redirect-middleware.yaml
+kubectl apply -f k3s-files/ecom-pv.yaml
+
+echo "Deployment completed!"
+echo " Access your app at: https://ecom.saurabhmate.cloud"
---
+++ terraform/main.tf
@@ -0,0 +1,116 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0f918f7e67a3323f0"
+  instance_type = "t3.large"
+  vpc_security_group_ids = [aws_security_group.TF_SG.id]
+  key_name = "k3s"
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 50
+  }
+
+  tags = {
+    Name = "saurabh-server"
+  }
+}
+
+resource "aws_security_group" "TF_SG" {
+  name        = "security group using terraform"
+  description = "security group using terraform"
+  vpc_id      = "vpc-06472eb98932e554e"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+ingress {
+    description      = "tcp"
+    from_port        = 6443
+    to_port          = 6443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "saurabh-server"
+  }
+}
+
+resource "local_file" "inventory" {
+  filename = "ansible/inventory.ini"
+  content  = <<-EOT
+    [server]
+    ${aws_instance.app_server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/saurabh/Downloads/Pem-files/k3s.pem
+  EOT
+}
+
+resource "cloudflare_record" "terraform_subdomain" {
+  zone_id = var.cloudflare_zone_id
+  name    = "ecom"
+  value   = aws_instance.app_server.public_ip
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
+
+output "instance_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+output "subdomain_url" {
+  value = "http://ecom.saurabhmate.cloud"
+}
